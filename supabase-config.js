e8b70eb8a642bb7a78a3164813b8344f8c1fec1f
Tjorbinjo <95943374+Tjorbinjo@users.noreply.github.com>
2026-09-11 23:18:13 +0200

    Update Supabase anon key URL format

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = 'https://DEIN-PROJEKT.supabase.co';
-window.SUPABASE_ANON_KEY = 'DEIN-PUBsb_publishable_B4lgEDx3xSp3jmkwfEga1A_nCwLqKZcLIC-ANON-KEY';
+window.SUPABASE_ANON_KEY = 'https://keaxhhojzyenllmdcdrh.supabase.co/rest/v1/?apikey=YOUR_ANON_KEY';
